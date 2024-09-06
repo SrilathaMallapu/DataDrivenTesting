6d589dda8de812bcbc11d52d49547963f39592d2
automation <Haritha@DESKTOP-L1F3TE2>
2024-09-06 21:03:46 +0530

    New changes are made from the branch1

--- DataDrivenFramework/src/test/java/DataDrivenTest/JunitFrameWork.java
+++ DataDrivenFramework/src/test/java/DataDrivenTest/JunitFrameWork.java
@@ -5,6 +5,8 @@ public class JunitFrameWork {
 	// this is a framework comments
 	// this is a second comments to be committed
 	// this is fourth commit made by branch1 without latest code
+	// this is third comment from branch2 
+	// this is the 6th comment from branch1 without latest branch2 update
 	public void JunitTestMethod()
 	
 	{
