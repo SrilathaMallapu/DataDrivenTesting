811e57bfa968cf05bf736b595fce0af8e62e86af
automation <Haritha@DESKTOP-L1F3TE2>
2024-09-06 21:25:45 +0530

    new updates are added

--- DataDrivenFramework/src/test/java/DataDrivenTest/JunitFrameWork.java
+++ DataDrivenFramework/src/test/java/DataDrivenTest/JunitFrameWork.java
@@ -9,9 +9,10 @@ public class JunitFrameWork {
 	// this is the 6th comment from branch1 without latest branch2 update
 	// this is the 6th comment from branch1 without latest branch2 update
 	//this is commit made by branch2 to check the pulling one
+	//Adding comment to push latest changes to branch2
 
 	public void JunitTestMethod()
-	
+
 	{
 		System.out.println("THis is test Method");
 
