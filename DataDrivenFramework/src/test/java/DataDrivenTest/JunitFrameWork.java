8d002e3204585829300f8ef88037c3dda39132f5
automation <Haritha@DESKTOP-L1F3TE2>
2024-09-06 21:17:35 +0530

    messages ared added

--- DataDrivenFramework/src/test/java/DataDrivenTest/JunitFrameWork.java
+++ DataDrivenFramework/src/test/java/DataDrivenTest/JunitFrameWork.java
@@ -6,16 +6,10 @@ public class JunitFrameWork {
 	// this is a second comments to be committed
 	// this is fourth commit made by branch1 without latest code
 	// this is third comment from branch2 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 	// this is the 6th comment from branch1 without latest branch2 update
->>>>>>> branch1
-=======
 	// this is the 6th comment from branch1 without latest branch2 update
 	//this is commit made by branch2 to check the pulling one
 
->>>>>>> branch1
 	public void JunitTestMethod()
 	
 	{
