6a07b5a71b1f8d53a2e02d9ae93bab2187c11142
automation <Haritha@DESKTOP-L1F3TE2>
2024-09-06 20:14:17 +0530

    A new comment is added to Junit Class

--- DataDrivenFramework/src/test/java/DataDrivenTest/JunitFrameWork.java
+++ DataDrivenFramework/src/test/java/DataDrivenTest/JunitFrameWork.java
@@ -3,7 +3,9 @@ package DataDrivenTest;
 public class JunitFrameWork {
 
 	// this is a framework comments
+	// this is a second comments to be committed
 	public void JunitTestMethod()
+	
 	{
 		System.out.println("THis is test Method");
 
